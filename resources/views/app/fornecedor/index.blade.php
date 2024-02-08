243478fa5f003ef631d2db1e38ec967b4b0b4b63
Marcos César Domingues <dfenixweb@gmail.com>
2024-02-08 11:50:44 -0300

    for in blade php

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -7,32 +7,22 @@
     @endphp
 </h3>
 @isset($fornecedores)
-    <h3>
-        Fornecedor: {{ $fornecedores[0]['nome'] }}
-    </h3>
-    <h3>
-        Status:{{ $fornecedores[0]['status'] }}
-    </h3>
-    <h3>
-        CNPJ:{{ $fornecedores[1]['cnpj'] ?? 'Dado nao preenchido' }}
-    </h3>
-    <h3>
-        Telefone: {{ $fornecedores[1]['ddd'] ?? '' }} {{ $fornecedores[1]['telefone'] ?? '' }}
-        @switch($fornecedores[0]['ddd'])
-            @case('11')
-                São Paulo
-            @break
-
-            @case('32')
-                Juiz de fora
-            @break
-
-            @case('85')
-                Fortaleza
-            @break
-
-            @default
-                Estado nao indent
-        @endswitch
-    </h3>
+    @for ($i = 0; isset($fornecedores[$i]); $i++)
+        <h3>
+            Fornecedor: {{ $fornecedores[$i]['nome'] ?? '' }}
+        </h3>
+        <h3>
+            Status: {{ $fornecedores[$i]['status'] ?? '' }}
+        </h3>
+        <h3>
+            CNPJ: {{ $fornecedores[$i]['cnpj'] ?? '' }}
+        </h3>
+        <h3>
+            DDD: {{ $fornecedores[$i]['ddd'] ?? '' }}
+        </h3>
+        <h3>
+            Telefone: {{ $fornecedores[$i]['telefone'] ?? '' }}
+        </h3>
+        <hr>
+    @endfor
 @endisset
